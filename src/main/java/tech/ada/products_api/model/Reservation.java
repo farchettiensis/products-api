7bf0f91be7855c495a48f85cbd6d869a983ed7d4
farchettiensis <farchettiensis@users.noreply.github.com>
2024-10-28 23:39:14 -0300

    Adds Reservation and ReservationDTO

--- src/main/java/tech/ada/products_api/model/Reservation.java
+++ src/main/java/tech/ada/products_api/model/Reservation.java
@@ -1,4 +1,29 @@
 package tech.ada.products_api.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@Setter
 public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String customerName;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private LocalTime time;
+
+    @Column
+    private int tableNumber;
 }
